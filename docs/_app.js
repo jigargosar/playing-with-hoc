f9e4cea563e867d303ab4000cb95676bdc6d2e95
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-28 13:10:09 +0530

    init

--- docs/_app.js
+++ docs/_app.js
@@ -1,5 +1,10 @@
 import React from 'react'
+// noinspection ES6UnusedImports
+import * as scope from 'rebass'
 import {SidebarLayout} from '@compositor/x0/components'
+// noinspection ES6UnusedImports
+import theme from './_theme'
+
 
 export default props =>
   <SidebarLayout {...props} title={`The name's Order... Higher Order`}/>
