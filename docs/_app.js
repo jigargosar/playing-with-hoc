ac037d3d04c65d299c859b7e1aff6b9abcd05c5b
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-28 13:33:58 +0530

    init

--- docs/_app.js
+++ docs/_app.js
@@ -4,7 +4,30 @@ import * as scope from 'rebass'
 import {SidebarLayout} from '@compositor/x0/components'
 // noinspection ES6UnusedImports
 import theme from './_theme'
+import {sortBy} from "ramda";
 
+const navOrder = [
+  'introduction',
+  'hoc-wat'
+]
+const pageNames = {
+  index: 'Home',
+  'hoc-wat':'HOC WAT!!!',
+  'cli-options': 'CLI Options'
+}
+
+const sortRoutes = routes => [
+  ...sortBy(a => {
+    const i = navOrder.indexOf(a.name)
+    return i < 0 ? Infinity : i
+  }, routes)
+].map(route => {
+  if (!pageNames[route.name]) return route
+  return {
+    ...route,
+    name: pageNames[route.name]
+  }
+})
 
 export default props =>
   <SidebarLayout {...props} title={`Higher Order C... !!!WAT!!!`}/>
