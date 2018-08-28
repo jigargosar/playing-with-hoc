3b90d3362ea42c97d4befb41c8972e02367ba095
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-28 12:29:25 +0530

    init

--- docs/demo.js
+++ docs/demo.js
@@ -1,9 +1,7 @@
 import React, {Fragment} from 'react'
 import {LivePreview} from '@compositor/x0/components'
 
-const code = `
-<button>Hello</button>
-`
+const code = `<button>Hello</button>`
 
 export default class extends React.Component {
   render() {
@@ -11,7 +9,6 @@ export default class extends React.Component {
       <Fragment>
         <h1>Demo</h1>
         <p>WAT!!!</p>
-        export default props =>
         <LivePreview
           code={code}
         />
