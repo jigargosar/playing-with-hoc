42821e6619f18e34058fc0eaeeb22e81149cfd50
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-28 13:44:40 +0530

    init

--- docs/_app.js
+++ docs/_app.js
@@ -1,8 +1,14 @@
+//
+// noinspection ES6UnusedImports,NpmUsedModulesInstalled,ES6CheckImport
 import React from 'react'
-// noinspection ES6UnusedImports
+// noinspection ES6UnusedImports,NpmUsedModulesInstalled,ES6CheckImport
+// noinspection ES6UnusedImports,NpmUsedModulesInstalled,ES6CheckImport
 import * as scope from 'rebass'
-import {SidebarLayout} from '@compositor/x0/components'
-// noinspection ES6UnusedImports
+import {Box, Container, Flex, Provider as RebassProvider} from 'rebass'
+// noinspection ES6UnusedImports,NpmUsedModulesInstalled,ES6CheckImport
+import {Link} from 'react-router-dom'
+//
+import {ScopeProvider, SidebarLayout} from '@compositor/x0/components'
 import theme from './_theme'
 import {sortBy} from "ramda";
 
@@ -29,5 +35,29 @@ const sortRoutes = routes => [
   }
 })
 
-export default props =>
-  <SidebarLayout {...props} title={`Higher Order C... !!!WAT!!!`}/>
+const LandingLayout = SidebarLayout
+
+export default props => {
+  const {
+    routes,
+    route,
+  } = props
+  const { layout } = (route && route.props) || {}
+
+  const Layout = layout === 'landing'
+    ? LandingLayout
+    : SidebarLayout
+
+  const nav = sortRoutes(routes)
+
+  return (
+    <ScopeProvider scope={scope}>
+      <RebassProvider theme={theme}>
+        <Layout
+          {...props}
+          routes={nav}
+          logo={<X0 size={24} color='magenta' />}
+        />
+      </RebassProvider>
+    </ScopeProvider>)
+}
