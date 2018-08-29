08f20d44b5be9c2cfe9430654880a00a741ca46b
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-29 16:16:57 +0530

    init

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,8 +3,10 @@ import "tachyons"
 import "../index.css"
 import {loadButtonStories} from "./button.stories";
 import {loadTableStories} from "./Table.stories";
+import {loadTableHOCStories} from "./TableHOC.stories";
 // import {linkTo} from '@storybook/addon-links';
 
 
+loadTableHOCStories();
 loadButtonStories();
 loadTableStories();
