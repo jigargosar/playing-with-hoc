87ff61d3e887ed675ddd784b8752546a9f331f96
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-29 16:19:31 +0530

    init

--- src/stories/TableHOC.stories.js
+++ src/stories/TableHOC.stories.js
@@ -1,29 +1,33 @@
 import {storiesOf} from '@storybook/react'
-import React from 'react'
+import React, {Fragment} from 'react'
 import Table from '../components/Table'
 
-export function loadTableHOCStories() {
-  storiesOf('HOC/Table', module)
-    .add('with basic content', () => (
-      <Table>
-        <caption>A Basic table</caption>
-        <thead>
-          <tr>
-            <th>Chars</th>
-            <th>Popularity</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>3</td>
-            <td>0.7</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>0.4</td>
-          </tr>
-        </tbody>
-      </Table>
-    ))
+function renderRows() {
+  return (
+    <Fragment>
+      <tr>
+        <td>3</td>
+        <td>0.7</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>0.4</td>
+      </tr>
+    </Fragment>
+  )
+}
 
+export function loadTableHOCStories() {
+  storiesOf('HOC/Table', module).add('with basic content', () => (
+    <Table>
+      <caption>A Basic table</caption>
+      <thead>
+        <tr>
+          <th>Chars</th>
+          <th>Popularity</th>
+        </tr>
+      </thead>
+      <tbody>{renderRows()}</tbody>
+    </Table>
+  ))
 }
