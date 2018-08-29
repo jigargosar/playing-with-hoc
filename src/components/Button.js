d06c81ffdc03da0325f7bd916b1fba5325059688
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-29 15:26:42 +0530

    init

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,21 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
 class Button extends Component {
   render() {
     return (
-      <div>
-
-      </div>
-    );
+      <button
+        {...this.props}
+        style={{
+          background: 'transparent',
+          border: 'none',
+          cursor:'pointer'
+        }}
+      />
+    )
   }
 }
 
-Button.propTypes = {children: PropTypes.node.isRequired,};
+Button.propTypes = { children: PropTypes.node.isRequired }
 
-export default Button;
+export default Button
