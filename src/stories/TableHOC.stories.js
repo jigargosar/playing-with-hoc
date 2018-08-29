a325ce9799426f293b98e2d810efbff444ca106c
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-29 16:23:35 +0530

    init

--- src/stories/TableHOC.stories.js
+++ src/stories/TableHOC.stories.js
@@ -1,9 +1,14 @@
 import {storiesOf} from '@storybook/react'
 import React, {Fragment} from 'react'
 import Table from '../components/Table'
+import {times} from 'ramda'
 
 function createPersonRow() {
-  return {name:"Foo", balance:1000}
+  return { name: 'Foo', balance: 1000 }
+}
+
+function allRows() {
+  return times(createPersonRow, 100)
 }
 
 function renderRows() {
@@ -13,10 +18,14 @@ function renderRows() {
         <td>3</td>
         <td>0.7</td>
       </tr>
-      <tr>
-        <td>3</td>
-        <td>0.4</td>
-      </tr>
+      {allRows().map(row => {
+        return (
+          <Fragment>
+            <td>{`${row.name}`}</td>
+            <td>{`${row.balance}`}</td>
+          </Fragment>
+        )
+      })}
     </Fragment>
   )
 }
