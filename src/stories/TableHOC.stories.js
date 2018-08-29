2a2253a2d79a943c41e95f2eb81bf35af8fd17ed
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-29 16:18:12 +0530

    init

--- src/stories/TableHOC.stories.js
+++ src/stories/TableHOC.stories.js
@@ -25,40 +25,5 @@ export function loadTableHOCStories() {
         </tbody>
       </Table>
     ))
-    .add('with merged cells', () => (
-      <Table>
-        <caption>table with merged cells</caption>
-        <colgroup>
-          <col />
-          <col />
-          <col />
-          <col style={{ backgroundColor: 'bisque' }} />
-        </colgroup>
-        <thead>
-          <tr>
-            <td rowSpan={2} />
-            <th colSpan={2}>Average</th>
-            <th rowSpan={2}>Red eyes</th>
-          </tr>
-          <tr>
-            <th>Height</th>
-            <th>Weight</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th>Males</th>
-            <td>1.9</td>
-            <td>0.003</td>
-            <td>40%</td>
-          </tr>
-          <tr>
-            <th>Females</th>
-            <td>1.7</td>
-            <td>0.002</td>
-            <td>43%</td>
-          </tr>
-        </tbody>
-      </Table>
-    ))
+
 }
