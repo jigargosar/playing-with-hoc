b2fa708372dcd9beec68bdd0d312143427209041
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-30 14:05:36 +0530

    init

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,13 @@
-import {configure} from '@storybook/react';
+import {addDecorator, configure} from '@storybook/react'
+import {setConsoleOptions, withConsole} from '@storybook/addon-console'
+
+setConsoleOptions({
+  panelExclude: [/\[HMR]/],
+})
+addDecorator((storyFn, context) => withConsole()(storyFn)(context))
 
 function loadStories() {
-  require('../src/stories');
+  require('../src/stories')
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
