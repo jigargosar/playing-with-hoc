7a8877473b9281c9e1c1c72cd8b517944a33b6ad
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-30 14:26:32 +0530

    init

--- src/stories/acceptance/App-1.stories.js
+++ src/stories/acceptance/App-1.stories.js
@@ -5,20 +5,19 @@ import Table from '../../styled-components/Table'
 storiesOf('Acceptance|App 1', module).add('with hardcoded table rows', () => (
   <Table>
     <thead>
-      {
-        <tr>
-          <th>Name</th>
-          <th>Job Title</th>
-          <th>Salary</th>
-        </tr>
-      }
+      <tr>
+        <th>Name</th>
+        <th>Job Title</th>
+        <th>Salary</th>
+      </tr>
     </thead>
     <tbody>
       <tr>
         <td>Mr. Foo Jr.</td>
         <td>Plumber</td>
         <td>$10,000.00</td>
-      </tr><tr>
+      </tr>
+      <tr>
         <td>Mr. Bar</td>
         <td>CEO</td>
         <td>$1.00</td>
