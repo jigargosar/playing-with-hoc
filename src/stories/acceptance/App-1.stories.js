da5e7ce3836d0bed619c8751b9809156413abe26
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-30 15:17:34 +0530

    init

--- src/stories/acceptance/App-1.stories.js
+++ src/stories/acceptance/App-1.stories.js
@@ -1,27 +1,54 @@
 import {storiesOf} from '@storybook/react'
 import React from 'react'
 import Table from '../../styled-components/Table'
+import SimpleTabs from '../../components/SimpleTabs'
 
 storiesOf('Acceptance|App 1', module).add('with hardcoded table rows', () => (
-  <Table>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Job Title</th>
-        <th>Salary</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Mr. Foo Jr.</td>
-        <td>Plumber</td>
-        <td>$10,000.00</td>
-      </tr>
-      <tr>
-        <td>Mr. Bar</td>
-        <td>CEO</td>
-        <td>$1.00</td>
-      </tr>
-    </tbody>
-  </Table>
+  <SimpleTabs
+    tabHeaderList={['First Table', 'Second Table']}
+    tabContentList={[
+      <Table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job Title</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Mr. Foo Jr.</td>
+            <td>Plumber</td>
+            <td>$10,000.00</td>
+          </tr>
+          <tr>
+            <td>Mr. Bar</td>
+            <td>CEO</td>
+            <td>$1.00</td>
+          </tr>
+        </tbody>
+      </Table>,
+      <Table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job Title</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Mr. Foo Jr.</td>
+            <td>Plumber</td>
+            <td>$10,000.00</td>
+          </tr>
+          <tr>
+            <td>Mr. Bar</td>
+            <td>CEO</td>
+            <td>$1.00</td>
+          </tr>
+        </tbody>
+      </Table>,
+    ]}
+  />
 ))
